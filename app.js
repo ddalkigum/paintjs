2a55e4438ca772225b7ea6f40b9e8e9fc2dee763
김준형 <sol35352000@gmail.com>
2020-06-12 02:42:55 +0900

    paint

--- app.js
+++ app.js
@@ -1,9 +1,76 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColors");
+const range =document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
+canvas.size = 700;
+ctx.strokeStyle = "#2c2c2c";
+ctx.linewidth = 2.5;
+
+let painting = false;
+let filling =false;
+
+function stopPainting(){
+    painting = false;
+}
+function startPainting(){
+    painting = true;
+}
 function onMouseMove(event){
-    console.log(event);
+ const x = event.offsetX;
+ const y = event.offsetY;
+ if(!painting){
+     ctx.beginPath();
+     ctx.moveTo(x, y);
+ } else{
+     ctx.lineTo(x, y);
+     ctx.stroke();
+ }
+ 
 }
 
-if (canvas){
+
+function onMouseUp(event){
+  stopPainting();
+}
+function onMouseLeave(event){
+    stopPainting();
+}
+
+function handleColorClick(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle =  color;
+    ctx.fillstyle = color;   
+}
+
+function handleRangeChange(event){
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
+function handleModeClick() {
+    if (filling === true) {
+      filling = false;
+      mode.innerText = "Fill";
+    } else {
+      filling = true;
+      mode.innerText = "Paint";
+    }
+  }
+if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting)
+}
+Array.from(colors).forEach(color => 
+    color.addEventListener("click",handleColorClick));
+
+if(range){
+    range.addEventListener("input", handleRangeChange);
+}
+
+if(mode){
+    mode.addEventListener("click",handleModeClick)
 }
